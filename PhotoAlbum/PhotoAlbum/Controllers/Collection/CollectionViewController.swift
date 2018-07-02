8478a365cdae2d6dd56ae688237a486f4b2b9094
Julian Lima <julian-lima@live.com>
2018-07-02 19:13:00 -0300

    Modifications in CollectionViewController: - split extension UICollectionViewDataSource/UICollectionViewDelegateFlowLayout - add section header - implement logic for load every cell

--- PhotoAlbum/PhotoAlbum/Controllers/Collection/CollectionViewController.swift
+++ PhotoAlbum/PhotoAlbum/Controllers/Collection/CollectionViewController.swift
@@ -44,7 +44,7 @@ class CollectionViewController: UIViewController {
     }
     
 }
-extension CollectionViewController : UICollectionViewDataSource, UICollectionViewDelegateFlowLayout{
+extension CollectionViewController : UICollectionViewDataSource{
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return photosCollectionViewModel.numberOfItemsInSection(section: section)
@@ -52,10 +52,44 @@ extension CollectionViewController : UICollectionViewDataSource, UICollectionVie
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         return photosCollectionViewModel.totalSections()
     }
+    //indexPath starts in 0 and ids of photos starts in 1 so indexPath.item + 1
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellid", for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellid", for: indexPath) as! PhotoViewCell
+        cell.setupCell(with: .Collection)
+        if photosCollectionViewModel.items[indexPath.section+1] != nil{
+            let list = photosCollectionViewModel.items[indexPath.section+1]
+            cell.loadPhoto(photo: (list?[indexPath.item])!,type: PhotoNames.thumbnailUrl, completionHandler: {_ in})
+        }else{
+            print("Fail when loading a cell IndexPath:(Section: \(indexPath.section+1), Item: \(indexPath.item)")
+        }
         return cell
     }
-    
-    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        // Dequeue Reusable Supplementary View
+        if let supplementaryView = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionElementKindSectionHeader, withReuseIdentifier: "header", for: indexPath) as? SectionHeader {
+            // Configure Supplementary View
+            supplementaryView.setHeader(name: "Album \(indexPath.section+1)")
+            return supplementaryView
+        }
+        fatalError("Unable to Dequeue Reusable Supplementary View")
+    }
+
+}
+extension CollectionViewController:  UICollectionViewDelegateFlowLayout{
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return CGSize(width: collectionView.bounds.width, height: 70)
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0.0
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 5.0
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = photosCollectionView.bounds.width / 2 - 15
+        return CGSize(width: width, height: width)
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsetsMake(10, 10, 10, 10)
+    }
 }

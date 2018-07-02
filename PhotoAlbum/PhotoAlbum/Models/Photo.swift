2d46640b44b286b07e2b0e0419d8a66f97b64a48
Julian Lima <julian-lima@live.com>
2018-07-02 18:07:16 -0300

    - Add APIHelper - Add Model - Photo

---
+++ PhotoAlbum/PhotoAlbum/Models/Photo.swift
@@ -0,0 +1,13 @@
+//
+//  Photo.swift
+//  PhotoAlbum
+//
+//  Created by Julian Lima on 2/7/18.
+//  Copyright © 2018 Julian Lima. All rights reserved.
+//
+
+import UIKit
+
+class Photo: NSObject {
+
+}

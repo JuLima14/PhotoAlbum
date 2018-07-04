c3cf9c79a208899596cbe053e54fa1c249bfca2a
Julian Lima <julian-lima@live.com>
2018-07-04 11:12:09 -0300

    - implement ListViewModel

--- PhotoAlbum/PhotoAlbum/Controllers/List/ListViewModel.swift
+++ PhotoAlbum/PhotoAlbum/Controllers/List/ListViewModel.swift
@@ -8,6 +8,27 @@
 
 import UIKit
 
-class ListViewModel: NSObject {
-
+class ListViewModel {
+    
+    var items = [Int:[Photo]]()
+    
+    func getTotalNumberInSection(section: Int) -> Int{
+        if APIHelper.shared.items[section+1] != nil{
+            return (APIHelper.shared.items[section+1]?.count)!
+        }else{
+            return 0
+        }
+    }
+    func getTotalSections() -> Int{
+        if APIHelper.shared.items.count == 0{
+            return 1
+        }else{
+            return APIHelper.shared.items.count
+        }
+    }
+    func loadPhotos(completionHandler: @escaping((Bool)->Void)){
+        APIHelper.shared.request_photos { (response) in
+            completionHandler(response)
+        }
+    }
 }

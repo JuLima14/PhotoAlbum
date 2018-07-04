4a06a86f1e94d53d24fbd80cf96e6c50caf6881b
Julian Lima <julian-lima@live.com>
2018-07-04 11:09:26 -0300

    - add ListView - add ListViewController - add ListViewModel

---
+++ PhotoAlbum/PhotoAlbum/Controllers/List/ListView.swift
@@ -0,0 +1,21 @@
+//
+//  ListView.swift
+//  PhotoAlbum
+//
+//  Created by Julian Lima on 4/7/18.
+//  Copyright © 2018 Julian Lima. All rights reserved.
+//
+
+import UIKit
+
+class ListView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ PhotoAlbum/PhotoAlbum/Controllers/List/ListViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ListViewController.swift
+//  PhotoAlbum
+//
+//  Created by Julian Lima on 4/7/18.
+//  Copyright © 2018 Julian Lima. All rights reserved.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+
+}
